af5fe568d419e9d11b9339cecd8b2698a56c9fc2
Ryan Holmes <holmes.ryan.90@gmail.com>
2013-11-08 04:35:59 +0000

    more work on lpOffer class. Haven't really worked on the bpc part. mostly trying to find an organization of the data that I like.

--- lib/lpOffer.php
+++ lib/lpOffer.php
@@ -14,166 +14,120 @@
     the info for the entire LP Store. lpStore() will do one big 
     query to get all offers and 1 query to get required items, 
     and pass this info along to lpOffer()
+    
+    @todo: allow switching req and man costs between sell and buy?
 */
 
 class lpOffer {
     
-    public $offerDetails;
-    public $reqDetails; # null = standalone, array() = no items via lpStore
-    
-    public $offerID;
-    public $cached = false; # was pricing info cached for offer?
-    public $bpc    = false; # is offer a BPC?
-
     private $DB;
     private $emdr;
     
-    # init's class, setting barebone variables
-    function __construct($offerID, $emdr, $DB = null) {
+    ## Details from database
+    public $offerDetails;
+    public $reqDetails; # null = standalone, array() = no items via lpStore
+    
+    ## Properties of the offer itself
+    public $offerID;
+    public $cached   = false; # was pricing info cached for offer?
+    public $bpc      = false; # is offer a BPC?
+    public $price    = null;  # pricing info for item (if blueprint, pricing info of manufactured item)
+    public $timeDiff = null;  # between now and when price was cached
+    public $totalCost = 0;    # total cost of offer (init isk/req/man)
+    public $profit    = 0;    # profit of offer after subtraction total cost
+    public $lp2isk    = 0;    # isk/lp after all calculations
+    
+    ## Details for manufacturing
+    public $manDetails = array();
+    
+    public $noCache  = array(); # store typeID => name of items with no price cache
+    
+    /*
+        Class is initiated with very basic information. Absolutely no calculation 
+        is done at this point. This is becasue class may be initialized by itself 
+        or as part of lpStore, and lpStore injects offer details before calcs 
+        take place.
+        
+        lpOffer isn't meant to return anything, but rather collect info and store 
+        in class properties for access.        
+    */
+    public function __construct($offerID, $emdr, $DB = null) {
         $this->DB      = $DB;
         $this->emdr    = $emdr;
         $this->offerID = $offerID;
     }
     
-    # calculate lp offer with data available
-    function calc() {
-        
-        # use $this->*Details variables
-        # if no details, use $DB to find values
-        # reqDetails is null if lpOffer() isn't init through lpStore()
-    
+    /*
+        calc() takes care of initializing calculation functions
+        It is also responsible for gathering data for items if needed.
+    */
+    # use $mode to determine if it's sell/buy
+    public function calc($mode) {  
+        /*
+        if ($mode !== 'sell' || $mode !== 'buy') {
+            # @todo: do actual error
+            die('HORRIBLE ERROR: Market Mode not valid. Plz fix');
+        }
+        */
         if ($this->offerDetails === null) {
-            $this->offerDetails = $this->DB->qa('SELECT * FROM `lpOffers` WHERE `offerID` = ?', array($this->offerID))[0]; }
+            $this->offerDetails = $this->DB->qa($this->sql['oDetails'], array($this->offerID))[0]; }
+            
+        if ($this->reqDetails === null) {
+            $this->reqDetails = $this->DB->qa($this->sql['rDetails'], array($this->offerID)); }
+        
+        if (strstr($this->offerDetails['typeName'], " Blueprint")) {
+            # If this is a bpc, set the flag and run bpc function
+            $this->bpc = true; 
+            $this->bpc();
+        } else {
+            # if this is not a blueprint, go ahead and set price via given known typeID
+            try {
+                $price = new Price($this->emdr->get($this->offerDetails['typeID']));
+                $this->cached      = true;
+                $this->price       = $price->sell[0];
+                $this->totalVolume = $price->sell[1];
+                $this->timeDiff    = (time() - $price->generatedAt)/60/60; # time difference in hours
+            } catch (Exception $e) {
+                array_push($this->noCache, $this->offerDetails['typeName']); }
+        }
+        
+        foreach ($this->reqDetails AS &$reqItem) {
+            try {
+                $price = new Price($this->emdr->get($reqItem['typeID']));
+                $reqItem['price'] = $price->sell[0]; 
+            } catch (Exception $e) {
+                array_push($this->noCache, $reqItem['typeName']); }
+        }
+        
+        # calculate total cost
+        $this->totalCost = $this->offerDetails['iskCost'];
+        foreach ($this->reqDetails AS &$reqItem) {
+            $this->totalCost += ($reqItem['quantity'] * $reqItem['price']); }
+            
+        foreach ($this->manDetails AS &$manItem) {
+            $this->totalCost += ($manItem['quantity'] * $manItem['price']); }
+        
+        # calculate profits / isk/lp
+        $this->profit = ($this->price * $this->offerDetails['quantity'] - $this->totalCost);
+        $this->lp2isk = $this->profit / $this->offerDetails['lpCost']; 
+    }
 
-        if ($price = $this->emdr->get($this->offerDetails['typeID'])) {
-            $this->cache  = true;
-            $price        = json_decode($price, true);
-            $timeDiff     = (time() - $price['orders']['generatedAt'])/60/60; // time difference in hours
-        }
-        
+    private function totalCost() {
+        $this->totalCost = $this->offerDetails['iskCost'];
+        foreach ($this->reqDetails AS &$reqItem) {
+            $this->totalCost += ($reqItem['quantity'] * $reqItem['price']); }
+            
+        foreach ($this->manDetails AS &$manItem) {
+            $this->totalCost += ($manItem['quantity'] * $manItem['price']); }
     }
     
-    # Get pricing info for required items
-    # returns nothing, simply modifies existign variables
-    function reqItems() {
-        // todo: should we parse pricing data for required items with no cache of their own?
-        if ($this->reqDetails === false) {
-            return; }
-        else if ($this->reqDetails === null) {
-            # haven't yet gathered info
-        }
-        
-        foreach ($this->reqDetails AS $reqItem) {
-            if ($rprice = $emdr->get($reqItem['typeID'])) {
-                $rprice = json_decode($rprice, true);
-                $totalCost = $totalCost + ($rprice['orders']['sell'][0] * $reqItem['quantity']);
-                //array_push($req, $reqItem['quantity']." x ".$reqItem['typeName']); 
-            }
-        }
-    }
-    
-    
-    # holder for old code.
+    /*# holder for old code.
     function FIX(){
             $totalCost = $offer['iskCost'];
-            $req       = array(); // array that holds name of reuired items
-            $cached    = false;   // flag
-            $bpc       = false;   // flag
-
-            // get pricing info on item
-
-            
-            // set required items
-            if (isset($reqContainer[$offer['offerID']])){
-                $reqItems = $reqContainer[$offer['offerID']];}
-            else {
-                $reqItems = array(); }
 
             $manReqItems = array();
 
-            
-            // Blueprints are special fucking buterflies
-            if (strstr($offer['typeName'], " Blueprint")) {
-                $bpc       = true;
-                $name      = "1 x ".$offer['typeName']." Copy (".$offer['quantity']." run".($offer['quantity'] > 1 ? "s" : null).")"; 
-                $label     = 'BP';
-                $fresh     = array('info', 'Calculating with manufactured item');
-                $manTypeID = $DB->q1(' 
-                    SELECT      ProductTypeID  
-                    FROM        invBlueprintTypes
-                    WHERE       blueprintTypeID = ?', array($offer['typeID']));
-                
-                // set pricing info as the manufactured item
-                if ($price = $emdr->get($manTypeID)) {
-                    $price  = json_decode($price, true); 
-                    $timeDiff = (time() - $price['orders']['generatedAt'])/60/60; // time difference in hours
-                    $cached = true;
-                }
-                
-                // Here we merge bill of materials for blueprints (remembering to multiple qnt with # of BPC runs)
-                $manReqItems = array_merge(
-                    // Get minerals needed
-                    $DB->qa('
-                        SELECT t.typeID,
-                               t.typeName,
-                               ROUND(greatest(0,sum(t.quantity)) * (1 + (b.wasteFactor / 100))) * ? AS quantity
-                        FROM
-                          (SELECT invTypes.typeid typeID,
-                                  invTypes.typeName typeName,
-                                  quantity
-                           FROM invTypes,
-                                invTypeMaterials,
-                                invBlueprintTypes
-                           WHERE invTypeMaterials.materialTypeID=invTypes.typeID
-                            AND invBlueprintTypes.productTypeID = invTypeMaterials.typeID
-
-                             AND invTypeMaterials.TypeID=?
-                           UNION 
-                           SELECT invTypes.typeid typeid,
-                                        invTypes.typeName name,
-                                        invTypeMaterials.quantity*r.quantity*-1 quantity
-                           FROM invTypes,
-                                invTypeMaterials,
-                                ramTypeRequirements r,
-                                invBlueprintTypes bt
-                           WHERE invTypeMaterials.materialTypeID=invTypes.typeID
-                             AND invTypeMaterials.TypeID =r.requiredTypeID
-                             AND r.typeID = bt.blueprintTypeID
-                             AND r.activityID = 1
-                             AND bt.productTypeID=?
-                             AND r.recycle=1) t
-                        INNER JOIN invBlueprintTypes b ON (b.productTypeID = ?)
-
-                        GROUP BY t.typeid,
-                                 t.typeName', array($offer['quantity'], $manTypeID, $manTypeID, $manTypeID)),
-                    // Get extra items needed
-                    $DB->qa('
-                        SELECT t.typeID AS    typeID,
-                            t.typeName AS     typeName,
-                            (r.quantity * ?) AS quantity
-                        FROM ramTypeRequirements r,
-                            invTypes t,
-                            invBlueprintTypes bt,
-                            invGroups g
-                        WHERE r.requiredTypeID = t.typeID
-                        AND r.typeID = bt.blueprintTypeID
-                        AND r.activityID = 1
-                        AND bt.productTypeID = ?
-                        AND g.categoryID != 16
-                        AND t.groupID = g.groupID', array($offer['quantity'], $manTypeID))); // append material needs to req items      
-                
-                foreach ($manReqItems AS $reqItem) {
-                    if ($reqItem['quantity'] <= 0) {
-                        continue; }
-
-                    if ($rprice = $emdr->get($reqItem['typeID'])) {
-                        $rprice = json_decode($rprice, true);
-                        $totalCost = $totalCost + ($rprice['orders']['sell'][0] * $reqItem['quantity']);
-                    }
-                }
-                // one day this will display them all, but for now, just note that materials are needed...
-                array_push($req, "Manufacturing Materials");
-            }     
+               
             else {
                 $name = $offer['quantity']." x ".$offer['typeName']; }
                
@@ -185,6 +139,94 @@ class lpOffer {
                 $lp2isk = $profit / $offer['lpCost']; 
             }
     }
+    */
+    /*
+        bpc() finds the pricing info for the manufactured item, which lpOffer()
+        will use to calculate isk/lp. it also sets required building materials
+    */
+    private function bpc() {
+        if (!$this->bpc) { return; } # Something's gone wrong, don't do this if not a BPC
+        
+        # Do this in template
+        // $name =  "1 x ".$offer['typeName']." Copy (".$offer['quantity']." run".($offer['quantity'] > 1 ? "s" : null).")"; 
+
+        $manTypeID = $DB->q1($this->sql['manTypeID'], array($offer['typeID']));
+        
+        // set pricing info as the manufactured item
+        if ($price = $emdr->get($manTypeID)) {
+            $price  = json_decode($price, true); 
+            $timeDiff = (time() - $price['orders']['generatedAt'])/60/60; // time difference in hours
+            $cached = true;
+        }
+        
+        // Here we merge bill of materials for blueprints (remembering to multiple qnt with # of BPC runs)
+        $manReqItems = array_merge(
+            // Get minerals needed
+            $DB->qa($this->sql['manMinerals'], array($offer['quantity'], $manTypeID, $manTypeID, $manTypeID)),
+            // Get extra items needed
+            $DB->qa($this->sql['manExtra'], array($offer['quantity'], $manTypeID))); // append material needs to req items      
+        
+        foreach ($manReqItems AS $reqItem) {
+            if ($reqItem['quantity'] <= 0) {
+                continue; }
+
+            if ($rprice = $emdr->get($reqItem['typeID'])) {
+                $rprice = json_decode($rprice, true);
+                $totalCost = $totalCost + ($rprice['orders']['sell'][0] * $reqItem['quantity']);
+            }
+        }
+        // one day this will display them all, but for now, just note that materials are needed...
+        array_push($req, "Manufacturing Materials");
+  
+    }
+    
+    /*
+        Having SQL throughout the class is ugly as shit. 
+        @todo: look into possibly setting up class full of SQL const and call from SQL::QUERYNAME
+    */
+    private $sql = array (
+        'oDetails'=>'SELECT a.*, b.`typeName` FROM `lpOffers` a NATURAl JOIN `invTypes` b WHERE `offerID` = ? LIMIT 0,1',
+        'rDetails'=>'SELECT a.*, b.typeName FROM lpOfferRequirements a NATURAL JOIN invTypes b WHERE `offerID` = ?',
+        'manTypeID'=>'SELECT `ProductTypeID` FROM `invBlueprintTypes` WHERE `blueprintTypeID` = ?',
+        
+        # I don't remember where I stole this from. It's one hell of a query tho
+        'manMinerals'=><<<'SQL'
+SELECT t.typeID, t.typeName, ROUND(greatest(0,sum(t.quantity)) * (1 + (b.wasteFactor / 100))) * ? AS quantity
+FROM
+   (SELECT invTypes.typeid typeID, invTypes.typeName typeName, quantity 
+    FROM invTypes, invTypeMaterials, invBlueprintTypes
+    WHERE invTypeMaterials.materialTypeID = invTypes.typeID AND
+          invBlueprintTypes.productTypeID = invTypeMaterials.typeID AND
+          invTypeMaterials.TypeID = ?
+    UNION 
+    SELECT invTypes.typeid typeid, invTypes.typeName name, invTypeMaterials.quantity * r.quantity * - 1 quantity
+    FROM invTypes, invTypeMaterials, ramTypeRequirements r, invBlueprintTypes bt 
+    WHERE invTypeMaterials.materialTypeID=invTypes.typeID AND
+          invTypeMaterials.TypeID =r.requiredTypeID AND
+          r.typeID = bt.blueprintTypeID AND
+          r.activityID = 1 AND 
+          bt.productTypeID = ? AND 
+          r.recycle = 1
+   ) t
+INNER JOIN invBlueprintTypes b ON (b.productTypeID = ?)
+GROUP BY t.typeid, t.typeName
+SQL
+,       'manExtra'=><<<'SQL'
+SELECT t.typeID AS typeID,  t.typeName AS     typeName,
+(r.quantity * ?) AS quantity
+FROM ramTypeRequirements r,
+invTypes t,
+invBlueprintTypes bt,
+invGroups g
+WHERE r.requiredTypeID = t.typeID
+AND r.typeID = bt.blueprintTypeID
+AND r.activityID = 1
+AND bt.productTypeID = ?
+AND g.categoryID != 16
+AND t.groupID = g.groupID
+SQL
+    );
+ 
 }
 
 ?>
