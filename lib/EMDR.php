b1b32f5c608fb17c560efca34c5acd7c51cbd145
Ryan Holmes <holmes.ryan.90@gmail.com>
2013-11-09 22:56:19 +0000

    added caching in EMDR class. Speed improvement will be effectively nothing for small stores, but the largest store saw a 0.3s decrease of calc time

--- lib/EMDR.php
+++ lib/EMDR.php
@@ -3,13 +3,31 @@
 /*
     Simple helper class that extends Redis
     Only does a connection to the Redis server and formats a key
+    
+    Added caching as well. Although accessing Redis values is very fast, doing 
+    them hundreds or times adds up. The class implements a simple caching in the 
+    form of 
+        
+        typeID => Redis string
+    
+    Whenever we request info with EMDR::get(), we first check to see if the 
+    price has already been fetched from the cache array and return it if true.
+    
+    This helps very large LP stores, as many of them have offers that use the 
+    same required items. This especially helps with blueprint calculations, as 
+    blueprints always use a basic mineral, and it's silly to call that same 
+    price data for the same mineral 18 different times for 18 blueprints. 
+    
+    Calculations for the larget LP Store at this time (1000180, 368 offers with 
+    18 BPCs, 752 calls to EMDR::get()) dropped by 0.3 seconds on average.
 */
 
 class EMDR extends Redis {
 
     public  $regionID;
     private $version;
-    
+    private $cache;
+
     public function __construct($regionID, $emdrVersion = 1)
     {
         parent::__construct();
@@ -21,8 +39,12 @@ class EMDR extends Redis {
     }
     
     public function get($typeID) {
-        $string = 'emdr-'.$this->version.'-'.$this->regionID.'-'.$typeID;
-        return parent::get($string);
+        if (isset($this->cache[$typeID])) {
+            return $this->cache[$typeID]; }
+        else {
+            $string = 'emdr-'.$this->version.'-'.$this->regionID.'-'.$typeID;
+            return $this->cache[$typeID] = parent::get($string);
+        }
     }
 }
 
