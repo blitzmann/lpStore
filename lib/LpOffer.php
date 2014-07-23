0ba980cb0e923e6d22faca699b7b6467c4004bb4
Ryan Holmes <holmes.ryan.90@gmail.com>
2014-07-23 04:03:18 +0000

    Updated to Crius

--- lib/LpOffer.php
+++ lib/LpOffer.php
@@ -1,27 +1,27 @@
 <?php
 
 /*
-    Class can either be initialized as a stand alone object, 
-    in which case the class will pull the data from the DB and calculate offer 
+    Class can either be initialized as a stand alone object,
+    in which case the class will pull the data from the DB and calculate offer
     details, or it can be initialized via lpStore in which case lpStore will
     supply details for the offer and lpOffer simply calculates prices.
-    
-    The reason for this is to cut down on the number of queries. 
-    If we initialized lpOffer() by itself for every LP Store 
-    offer, that's potentially hundreds of queries just to get 
-    the info for the entire LP Store. lpStore() will do one big 
-    query to get all offers and 1 query to get required items, 
+
+    The reason for this is to cut down on the number of queries.
+    If we initialized lpOffer() by itself for every LP Store
+    offer, that's potentially hundreds of queries just to get
+    the info for the entire LP Store. lpStore() will do one big
+    query to get all offers and 1 query to get required items,
     and pass this info along to lpOffer()
 */
 
 class LpOffer {
 
     private $bpcCache;
-    
+
     ## Details from database
     public $offerDetails;
     public $reqDetails;
-    
+
     ## Properties of the offer itself
     public $offerID;
     public $cached   = false; # was pricing info cached for offer?
@@ -32,21 +32,21 @@ class LpOffer {
     public $margin    = 0;    # profit of offer after subtraction total cost
     public $profit    = 0;    # pure profit of selling the offer
     public $lp2isk    = 0;    # isk/lp after all calculations
-    public $totVolume = 0; 
+    public $totVolume = 0;
     ## Details for manufacturing
     public $manDetails = array();
     public $manTypeID = null;
-    
+
     public $noCache  = array(); # store typeID => name of items with no price cache
-    
+
     public function __get($field) {
         return $this->offerDetails[$field]; }
-    
+
     /*
-        Class is initiated with basic information. lpStore is able to pass along 
-        details of the offer, in which case {offer|req}Details are set to 
-        something other than null. 
-        
+        Class is initiated with basic information. lpStore is able to pass along
+        details of the offer, in which case {offer|req}Details are set to
+        something other than null.
+
         lpOffer is meant to collect info and store in class properties for access.
     */
     public function __construct($offerID, $offerDetails = null, $reqDetails = null) {
@@ -55,27 +55,27 @@ class LpOffer {
         $this->reqDetails   = $reqDetails;
         # @todo: list orderDetails to variables ?
     }
-    
+
     /*
         calc() takes care of initializing calculation functions
         It is also responsible for gathering data for items if needed.
 
         Returns self
     */
-    
+
     public function calc() {
-        try {            
+        try {
             if ($this->offerDetails === null) {
                 $this->offerDetails = Db::q(Sql::oDetails, array($this->offerID))[0]; }
-            
+
             if (empty($this->offerDetails)) { throw Exception('No offer details available.'); }
-            
+
             if ($this->reqDetails === null) {
                 $this->reqDetails = Db::q(Sql::rDetails, array($this->offerID)); }
-            
+
             if (strstr($this->offerDetails['typeName'], " Blueprint")) {
                 # If this is a bpc, set the flag and run bpc function
-                $this->bpc = true; 
+                $this->bpc = true;
                 $this->bpcCalc();
             } else {
                 # if this is not a blueprint, go ahead and set price via given known typeID
@@ -88,24 +88,24 @@ class LpOffer {
                 } catch (Exception $e) {
                     array_push($this->noCache, $this->offerDetails['typeName']); }
             }
-            
+
             foreach ($this->reqDetails AS &$reqItem) {
                 try {
                     $price = new Price(Emdr::get($reqItem['typeID']));
                     $reqItem['price']    = $price->{Prefs::get('marketReq')}[0];
-                    $reqItem['totPrice'] = $reqItem['price']* $reqItem['quantity']; 
+                    $reqItem['totPrice'] = $reqItem['price']* $reqItem['quantity'];
                 } catch (Exception $e) {
                     array_push($this->noCache, $reqItem['typeName']); }
             }
-            
+
             # calculate total cost
             $this->totalCost = $this->offerDetails['iskCost'];
             foreach ($this->reqDetails AS &$reqItem) {
                 $this->totalCost += ($reqItem['quantity'] * $reqItem['price']); }
-                
+
             foreach ($this->manDetails AS &$manItem) {
                 $this->totalCost += ($manItem['totQty'] * $manItem['price']); }
-            
+
             # calculate profits / isk/lp
             $this->profit = $this->price * $this->offerDetails['quantity'];
             $this->margin = $this->profit - $this->totalCost;
@@ -113,7 +113,7 @@ class LpOffer {
         } catch (Exception $e) {
             die($e);
         }
-        
+
         return $this;
     }
 
@@ -123,16 +123,17 @@ class LpOffer {
     */
     private function bpcCalc() {
         if (!$this->bpc) { return; } # Something's gone wrong, don't do this if not a BPC
-        
+
         # Do this in template
-        // $name =  "1 x ".$offer['typeName']." Copy (".$offer['quantity']." run".($offer['quantity'] > 1 ? "s" : null).")"; 
+        // $name =  "1 x ".$offer['typeName']." Copy (".$offer['quantity']." run".($offer['quantity'] > 1 ? "s" : null).")";
 
         $this->manTypeID = Db::qColumn('
-            SELECT `ProductTypeID` 
-            FROM `invBlueprintTypes` 
-            WHERE `blueprintTypeID` = :typeID', 
+            SELECT `productTypeID`
+            FROM `industryActivityProducts`
+            WHERE `typeID` = :typeID
+            AND `activityID` = 1',
             array(':typeID' => $this->offerDetails['typeID']));
-        
+
         # set pricing info per the manufactured item
         try {
             $price = new Price(Emdr::get($this->manTypeID));
@@ -142,56 +143,56 @@ class LpOffer {
             $this->timeDiff    = (time() - $price->generatedAt)/60/60; # time difference in hours
         } catch (Exception $e) {
             array_push($this->noCache, $this->offerDetails['typeName']); }
-        
+
         # find cached result of BPC manufacturing materials
-        $this->manDetails = (new Query_OfferMaterials($this->manTypeID, $this->offerDetails['quantity']))->execute();
-        
+        $this->manDetails = (new Query_OfferMaterials($this->offerDetails['typeID'], $this->offerDetails['quantity']))->execute();
+
         # set market group to BPs (doesn't happen automatically since you can't find LP Store BPs on market)
         $this->offerDetails['marketRoot'] = '2';
     }
-    
+
     public function getSimilar() {
         return (new Query_OfferSimilar($this->offerDetails['typeID']))->execute();
     }
-    
+
     public function getStores() {
         return (new Query_OfferStores($this->offerID))->execute();
     }
-    
+
     public function getReqCount() {
         return count($this->reqDetails);
     }
-    
+
     public function getManCount() {
         return count($this->manDetails);
     }
-    
+
     /*
-        This gets the name of the item being used as the basis for pricing data. 
+        This gets the name of the item being used as the basis for pricing data.
         Basically, if blueprint, gets the name of the manufactured object
-        
+
         @todo: possibly find a better way to handle blueprints in general.
     */
     public function getProductName() {
         if ($this->bpc) {
             return str_replace('Blueprint', '', $this->typeName); }
-        
+
         return $this->typeName;
     }
-    
+
     /*
         This is the display name for the offer. If BPC, do a little bit of work
         (add # runs)
-        
-        @todo: Possibly replace offer qty with 1 for BPCs and add a different 
-        property to the class for # runs. This way, we can get rid of special 
+
+        @todo: Possibly replace offer qty with 1 for BPCs and add a different
+        property to the class for # runs. This way, we can get rid of special
         cases like this
-        
+
     */
     public function getDisplayName() {
         if ($this->bpc) {
             return '1 x '.$this->typeName.' Copy ('.$this->quantity.' run'.($this->quantity > 1 ? 's' : null).')'; }
-        
+
         return $this->quantity.'x '.$this->typeName;
     }
 }
--- lib/Query/OfferMaterials.php
+++ lib/Query/OfferMaterials.php
@@ -2,34 +2,31 @@
 
 class Query_OfferMaterials {
 
-    function __construct($productID, $qty = 1) {
-        $this->productID = $productID;
-        $this->qty       = $qty;
+    function __construct($typeID, $qty = 1) {
+        $this->typeID = $typeID;
+        $this->qty    = $qty;
 
-        # Redis DB specifically for BPC Material caching. 
+        # Redis DB specifically for BPC Material caching.
         # @todo: Set this as a singleton class?
         $this->bpcCache = new Redis();
         $this->bpcCache->connect('localhost', 6379);
         $this->bpcCache->select(Config::lpStoreRedis);
     }
-  
+
     /*
         Execute query to obtain both BPC materials requirements and 'extra' items
         for an item. Checks cache first, which is in the format of:
-        
-            Class:ProductID,Qty
 
-        Important to remember that it's the product ID, not the BPC ID, that 
-        is stored as key.
+            Class:bpcTypeID,Qty
 
         @todo: post-processing with icon path
         @return Returns array of rows
     */
     function execute() {
-        $key   = get_class().':'.$this->productID.','.$this->qty;
+        $key   = get_class().':'.$this->typeID.','.$this->qty;
         $cache = json_decode($this->bpcCache->get($key), true);
-        
-        if (empty($cache) || empty($cache['manDetails']) || $cache['version'] != Db::$dbName) { 
+
+        if (empty($cache) || empty($cache['manDetails']) || $cache['version'] != Db::$dbName) {
             $cache = array('version'=>Db::$dbName, 'manDetails'=>$this->performQuery());
             $this->bpcCache->set($key, json_encode($cache));
         }
@@ -38,119 +35,31 @@ class Query_OfferMaterials {
         foreach ($cache['manDetails'] AS &$manItem) {
             try {
                 $price = new Price(Emdr::get($manItem['typeID']));
-                $manItem['price']    = $price->{Prefs::get('marketMat')}[0]; 
-                $manItem['totPrice'] = $manItem['price']*$manItem['totQty']; 
+                $manItem['price']    = $price->{Prefs::get('marketMat')}[0];
+                $manItem['totPrice'] = $manItem['price']*$manItem['totQty'];
             } catch (Exception $e) {
                 array_push($this->noCache, $manItem['typeName']); }
         }
 
-        return $cache['manDetails']; 
+        return $cache['manDetails'];
     }
-    
-    
-    /*
-        Query materials for manufacturing a product. Assumes perfect skills. 
-        Included in results are qty for both single and total runs
-        
-        Blueprint are finicky things. There are some blueprints that pop up as having
-        material that are needed, only to be negated by another query. I think
-        I originally got this query from Fuzzy's Steve
 
-        @todo: look into making this not suck so bad
+
+    /*
+        Query materials for manufacturing a product. Assumes perfect skills.
+        Included in results are qty for both single and total runs
 
         @return Returns array of rows
     */
 
     function performQuery() {
-        $result1 = Db::q('
-            SELECT t.typeID,
-                   t.typeName,
-                   ROUND(greatest(0,sum(t.quantity)) * (1 + (b.wasteFactor / 100))) AS runQty
-            FROM
-              (SELECT invTypes.typeid typeID,
-                      invTypes.typeName typeName,
-                      quantity
-               FROM invTypes,
-                    invTypeMaterials,
-                    invBlueprintTypes
-               WHERE invTypeMaterials.materialTypeID=invTypes.typeID
-                AND invBlueprintTypes.productTypeID = invTypeMaterials.typeID
+        $result = Db::q('
+            SELECT a.materialTypeID as typeID, b.typeName, a.quantity as runQty, a.quantity*:bpcRuns AS totQty
+            FROM industryActivityMaterials a
+            JOIN invTypes b on (a.materialTypeID = b.typeID)
+            WHERE a.typeID=:typeID',
+            array(':typeID' => $this->typeID, ':bpcRuns'=>$this->qty));
 
-                 AND invTypeMaterials.TypeID=:productID
-               UNION 
-               SELECT invTypes.typeid typeid,
-                            invTypes.typeName name,
-                            invTypeMaterials.quantity*r.quantity*-1 quantity
-               FROM invTypes,
-                    invTypeMaterials,
-                    ramTypeRequirements r,
-                    invBlueprintTypes bt
-               WHERE invTypeMaterials.materialTypeID=invTypes.typeID
-                 AND invTypeMaterials.TypeID =r.requiredTypeID
-                 AND r.typeID = bt.blueprintTypeID
-                 AND r.activityID = 1
-                 AND bt.productTypeID=:productID
-                 AND r.recycle=1) t
-            INNER JOIN invBlueprintTypes b ON (b.productTypeID = :productID)
-
-            GROUP BY t.typeid,
-                     t.typeName', 
-            array(':productID' => $this->productID));
-        // Get extra items needed
-        $result2 = Db::q('
-            SELECT t.typeID AS    typeID,
-                t.typeName AS     typeName,
-                r.quantity AS runQty
-            FROM ramTypeRequirements r,
-                invTypes t,
-                invBlueprintTypes bt,
-                invGroups g
-            WHERE r.requiredTypeID = t.typeID
-            AND r.typeID = bt.blueprintTypeID
-            AND r.activityID = 1
-            AND bt.productTypeID = :productID
-            AND g.categoryID != 16
-            AND t.groupID = g.groupID',
-            array(':productID' => $this->productID));
-        
-        $mats = array();
-        foreach($result = array_merge($result1, $result2) AS &$item) {
-            # this sometimes happens for some reason
-
-            if ($item['runQty'] <= 0) { 
-              $item = null; continue;}
-
-            if (!isset($mats[$item['typeID']])) { $mats[$item['typeID']] = $item; $mats[$item['typeID']]['totQty'] = 0; }
-            
-            $mats[$item['typeID']]['totQty'] += $item['runQty'] * $this->qty;
-        }
-
-        return array_filter($mats);
+        return $result;
     }
-
-    /*
-        Old SQL that doesn't negate materials
-
-            SELECT typeID, typeName, SUM(quantity) * :quantity AS totQty, SUM(quantity) AS runQty
-            FROM (
-                SELECT t.typeID, t.typeName, ROUND(m.quantity * 1.1) as quantity
-                FROM invTypeMaterials AS m
-                INNER JOIN invTypes AS t ON m.materialTypeID = t.typeID
-                WHERE m.typeID = :productID
-                
-                UNION
-
-                SELECT t.typeID, t.typeName , r.quantity
-                FROM ramTypeRequirements r, invTypes t, invBlueprintTypes bt, invGroups g
-                WHERE 
-                    r.requiredTypeID = t.typeID AND
-                    r.typeID = bt.blueprintTypeID AND
-                    r.activityID = 1 AND
-                    g.categoryID != 16 AND 
-                    t.groupID = g.groupID AND
-                    bt.productTypeID = :productID 
-            ) foo
-            GROUP BY `typeID`
-    */
-    
 }
