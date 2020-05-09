81be344b879eddf6ea7069d286b0aa8d07db2d66
blitzmann <holmes.ryan.90@gmail.com>
2020-05-09 01:24:54 -0400

    This change was made years ago and never committed. I have no idea what it does, or what it attempted to fix. v0v

--- lib/Query/CorpStations.php
+++ lib/Query/CorpStations.php
@@ -6,29 +6,29 @@ class Query_CorpStations {
         $this->corpID = $corpID;
         $this->system = $system;
     }
-  
+
     /*
         Execute query to obtain all Stations for a corp
-        
+
         @return Returns array of rows
     */
     function execute() {
         $result = Db::q("
-                SELECT a.`stationName`, a.`stationID`, b.`security`, b.`solarSystemID`, c.`regionName`,
+                SELECT SQL_NO_CACHE a.`stationName`, a.`stationID`, b.`security`, b.`solarSystemID`, c.`regionName`,
                 (
-                    SELECT seq 
+                    SELECT seq
                     FROM graphJumps
-                    WHERE latch = 2
+                    WHERE latch = 'breadth_first'
                     AND origid = :origin
                     AND destid = b.`solarSystemID`
                     ORDER BY seq DESC
                     LIMIT 0,1
                 ) AS jumps
-                FROM `staStations` a 
+                FROM `staStations` a
                 INNER JOIN `mapSolarSystems` b ON (b.`solarSystemID` = a.`solarSystemID`)
                 INNER JOIN `mapRegions` c ON (a.`regionID` = c.`regionID`)
                 WHERE a.`corporationID` = :corpID
-                ORDER BY a.`stationName` ASC", 
+                ORDER BY a.`stationName` ASC",
                 array(':corpID'=>$this->corpID, ':origin'=>$this->system));
 
         return $result;
